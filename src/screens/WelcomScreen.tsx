54e7d6d042233eef7b21866c3608085f7d3e31ed
ftgeorge <ftgeorgean@gmail.com>
2023-10-30 09:41:10 +0100

    Feat: Updated the UI

--- src/screens/WelcomScreen.tsx
+++ src/screens/WelcomScreen.tsx
@@ -7,33 +7,28 @@ import foodmarketimg from "../assets/earth-3289810_1920.jpg"
 
 export default function WelcomeScreen() {
     return (
-        <View style={{ width: screenWidth, height: screenHeight, alignItems: 'center' }}>
-            <Text style={{ color: 'black', fontFamily: 'Raleway-SemiBold', fontSize: getFontSize(32), marginTop: '5%' }}>KARA</Text>
-            <Text style={{ color: '#4B4B4B', fontFamily: 'Raleway-Regular', fontSize: getFontSize(18) }}>Buy, Sell, Deliver</Text>
-            <ImageBackground source={foodmarketimg} imageStyle={{ borderRadius: 10 }} style={{ width: screenWidth * 0.8, height: screenHeight / 5, borderRadius: 10, marginTop: '7%' }}>
-                <View style={{ width: screenWidth * 0.8, height: screenHeight / 5, backgroundColor: 'rgba(52, 156, 104, 0.6)', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{backgroundColor:'#FFFFFF' ,width: screenWidth, height: screenHeight, alignItems: 'center' }}>
+            <Text style={{ color: 'black', fontFamily: 'Raleway-Bold', fontSize: getFontSize(32), marginTop: '5%' }}>KARA</Text>
+            <Text style={{ color: '#4B4B4B', fontFamily: 'Raleway-SemiBold', fontSize: getFontSize(18) }}>Buy, Sell, Deliver</Text>
+            <ImageBackground source={foodmarketimg} imageStyle={{ borderRadius: 10 }} style={{ width: screenWidth * 0.8, height: screenHeight / 5.2, borderRadius: 10, marginTop: '7%' }}>
+                <View style={{ width: screenWidth * 0.8, height: screenHeight / 5.2, backgroundColor: 'rgba(52, 156, 104, 0.6)', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={{ fontFamily: 'Raleway-SemiBold', color: 'white', fontSize: getFontSize(24) }}>Food Market</Text>
                 </View>
             </ImageBackground>
-            <ImageBackground source={foodmarketimg} imageStyle={{ borderRadius: 10 }} style={{ width: screenWidth * 0.8, height: screenHeight / 5, borderRadius: 10, marginTop: '7%' }}>
-                <View style={{ width: screenWidth * 0.8, height: screenHeight / 5, backgroundColor: 'rgba(238, 119, 74, 0.6)', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
+            <ImageBackground source={foodmarketimg} imageStyle={{ borderRadius: 10 }} style={{ width: screenWidth * 0.8, height: screenHeight / 5.2, borderRadius: 10, marginTop: '7%' }}>
+                <View style={{ width: screenWidth * 0.8, height: screenHeight / 5.2, backgroundColor: 'rgba(238, 119, 74, 0.6)', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={{ fontFamily: 'Raleway-SemiBold', color: 'white', fontSize: getFontSize(24) }}>Pharmaceuticals</Text>
                 </View>
             </ImageBackground>
 
-            <ImageBackground source={foodmarketimg} imageStyle={{ borderRadius: 10 }} style={{ width: screenWidth * 0.8, height: screenHeight / 5, borderRadius: 10, marginTop: '7%' }}>
-                <View style={{ width: screenWidth * 0.8, height: screenHeight / 5, backgroundColor: 'rgba(255, 255, 255, 0.6)', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
+            <ImageBackground source={foodmarketimg} imageStyle={{ borderRadius: 10 }} style={{ width: screenWidth * 0.8, height: screenHeight / 5.2, borderRadius: 10, marginTop: '7%' }}>
+                <View style={{ width: screenWidth * 0.8, height: screenHeight / 5.2, backgroundColor: 'rgba(255, 255, 255, 0.6)', borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={{ fontFamily: 'Raleway-SemiBold', color: 'white', fontSize: getFontSize(24) }}>Logistics</Text>
                 </View>
             </ImageBackground>
-            <TouchableOpacity style={{ backgroundColor: '#349C68', width: screenWidth * 0.8, height: screenHeight / 18, justifyContent: 'center', alignItems: 'center', borderRadius: 10, marginTop: "5%" }}>
+            <TouchableOpacity style={{ backgroundColor: '#349C68', width: screenWidth * 0.8, height: screenHeight / 16, justifyContent: 'center', alignItems: 'center', borderRadius: 10, marginTop: "10%", marginBottom:"10%" }}>
                 <Text style={{ color: 'white', fontFamily: 'Raleway-Regular', fontSize: getFontSize(18) }}>Login</Text>
             </TouchableOpacity>
-
-            <TouchableOpacity style={{flexDirection:'row', backgroundColor: '#E6E6E6', width: screenWidth * 0.8, height: screenHeight / 18, justifyContent: 'center', alignItems: 'center', borderRadius: 10, marginTop: "5%" }}>
-                <Text style={{ color: 'black', fontFamily: 'Raleway-Regular', fontSize: getFontSize(18) }}>Location</Text>
-            </TouchableOpacity>
-
         </View>
     );
 }
